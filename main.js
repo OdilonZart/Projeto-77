423117bc6ba069b88f14ed3c91a308fe19f905ac
OdilonZart <114754713+OdilonZart@users.noreply.github.com>
2023-02-06 14:43:52 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log("Hello World")
+var firstName="Odilon"
+var secondName="Zart"
+var fullName=firstName.concat(secondName)
+console.log(fullName)
+var anotherName=fullName.replace("Zart","Moura")
+var lengthName=fullName.length
+console.log(lengthName)
